d4e48630219bd142882e6e3e761c4cc447e61949
Alberto <alcs1491@gmail.com>
2025-07-16 19:35:39 -0600

    Version3

--- main.py
+++ main.py
@@ -15,18 +15,37 @@ class Program:
                 carrera=input("Ingrese la carrera: ")
                 cantCursos=int(input("Ingrese la cantidad de cursos: "))
                 for i in range(cantCursos):
+                    codigoCurso=i+1
                     curso=input("Ingrese el nombre del curso: ")
                     notaTarea=int(input("Ingrese nota tarea: "))
                     notaParcial=int(input("Ingrese nota parcial: "))
                     notaProyecto=int(input("Ingrese la note del proyecto: "))
-                    estudiantes[carnet]={"nombre" : nombre, "edad": edad, "carrera": carrera, "cursos": {"codigoCurso": i+1, "curso": curso, "notaTarea":notaTarea, "notaParcial": notaParcial, "notaProyecto": notaProyecto}}
+                    estudiantes[carnet]={"nombre" : nombre, "edad": edad, "carrera": carrera, "cursos": {"curso": curso, "notaTarea":notaTarea, "notaParcial": notaParcial, "notaProyecto": notaProyecto}}
                 print("\nEstudiante registrado con exito...")
                 input()
             elif opcion==2:
                 print("============ESTUDIANTES REGISTRADOS===========")
-                for j in len(estudiantes):
-                    print(estudiantes.items())
+                for carnet, datos in estudiantes.items():
+                    print(f"""Carnet: {carnet}
+Nombre: {datos["nombre"]}
+Edad: {datos["edad"]}
+Carrera: {datos["carrera"]}
+Cursos: 
+""")
+                    for codigoCurso, var in datos["cursos"].items():
+                        print(f"""Codigo de curso: {codigoCurso}
+Nombre del curso: {var["curso"]}
+Nota de Tarea: {var["notaTarea"]}
+Nota de Parcial: {var["notaParcial"]}
+Nota de Proyecto: {var["notaProyecto"]}""")
+
+
                 input()
+            elif opcion==3:
+                buscado=input("Ingrese carnet a buscar: ")
+                if buscado in estudiantes:
+                    datos= estudiantes[buscado]
+
 
 
 
